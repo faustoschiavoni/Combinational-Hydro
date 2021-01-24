bdc74e493db421d88ae5181542209199069fe96e
faustoschiavoni <64273539+faustoschiavoni@users.noreply.github.com>
2021-01-24 21:58:11 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from Risultati import Risultati
 Optimization_Goal = 'NPV'
 
 
-# Invoco la funzione che calcola la più grande Potenza_annua [MWh] --> se il numero di turbine A e B è fissato implice un maggiore NPV 
+# Invoco la funzione che massimizza il NPV
 Giorni, permutazioni, Combinazioni_portata, na, nb, A, Ap, Hnet, Hnetp, Fattore, Fattorep, Potenza_giorn, Potenza_scelta, Energia_annua, NPV = Conti(Optimization_Goal)
 
 # Risultati
